b3546b3e79bb8e52e3019971cb177aabf2b915d6
dyakonovr <dyakonov.rr@bk.ru>
2021-09-10 20:26:31 +0400

    add 3 sections and swiper-js

---
+++ src/js/components/idx-slider.js
@@ -0,0 +1,9 @@
+const idxSwiper = new Swiper('.projects__swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
