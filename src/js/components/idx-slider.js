77cb109b4a146f65a87cbf59f396bb12e7623d65
dyakonovr <dyakonov.rr@bk.ru>
2021-10-13 20:57:44 +0400

    added responsiveness on screens >768px and <1200px (index.html)

--- src/js/components/idx-slider.js
+++ src/js/components/idx-slider.js
@@ -1,11 +1,16 @@
 const idxSwiper = new Swiper('.projects__swiper', {
   slidesPerView: 2,
   spaceBetween: 15,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  loop: true,
+  breakpoints: {
+    992: {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    },
+  }
 });
