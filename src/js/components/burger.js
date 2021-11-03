c74c5958f135ae23559fd471a28939aa88f415b1
dyakonovr <dyakonov.rr@bk.ru>
2021-11-03 14:01:47 +0400

    made a preloader

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,13 +1,15 @@
 const burger = document.querySelector('.burger');
-const nav = document.querySelector('.hd-nav-js');
+const menuBlock = document.querySelector('.header__wrapper');
 const btns = document.querySelector('.header__btns');
+const nav = document.querySelector('.hd-nav-js');
 const subLists = document.querySelectorAll('.header__sub-list');
 
 if (burger) {
   burger.addEventListener('click', function () {
     burger.classList.toggle('burger-close');
-    nav.classList.toggle('header__nav--show');
+    menuBlock.classList.toggle('header__wrapper--show');
     btns.classList.toggle('show');
+    nav.classList.toggle('show');
     subLists.forEach(list => {
       list.classList.toggle('show');
     });
---
+++ src/js/components/preloader.js
@@ -0,0 +1,32 @@
+const preloader = document.querySelector('.preloader');
+const images = document.images;
+const imagesTotal = images.length;
+let imagesLoaded = 0;
+const percentDisplay = document.querySelector('.preloader__percent');
+const progressDisplay = document.querySelector('.preloader__progress');
+
+if (preloader) {
+
+  for (let i = 0; i < imagesTotal; i++) {
+    let imageClone = new Image();
+    imageClone.onload = imageLoaded;
+    imageClone.onerror = imageLoaded;
+    imageClone.src = images[i].src;
+  }
+
+  function imageLoaded() {
+    imagesLoaded++;
+    let percent = (((100 / imagesTotal) * imagesLoaded) << 0);
+    percentDisplay.innerHTML = percent + '%';
+    progressDisplay.style.width = percent + '%';
+
+
+    if (imagesLoaded >= imagesTotal) {
+      setTimeout(function () {
+        if (!preloader.classList.contains('preloader__done')) {
+          preloader.classList.add('preloader__done')
+        }
+      }, 1200)
+    }
+  }
+}
